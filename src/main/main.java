62678bdfeba881e0f244b8363a4a5128ccf63787
raulp <raulp@DESKTOP-9IMU10S>
2024-10-03 19:02:59 +0200

    8

--- src/main/main.java
+++ src/main/main.java
@@ -3,7 +3,7 @@ package main;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Esta es la septima versión");
+		System.out.println("Esta es la octava versión");
 	}
 
 }
